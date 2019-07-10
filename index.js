a3a962dadb3194a132aa76b30487d416f159c4f5
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-10 15:54:07 +0100

    Create custom logger middleware

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const userRoutes = require("./users/userRouter");
 const server = express();
 
 server.use(express.json());
+
+//custom middleware
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+
+  next();
+}
+
+server.use(logger);
+
 server.use(express.urlencoded({ extended: true }));
 
 server.use("/api", userRoutes);
@@ -14,11 +24,6 @@ server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
-
-// function logger(req, res, next) {
-
-// };
 
 server.listen(5000, () => {
   console.log("listening on 5000");
